3243a55b81480458a9ac1a796085fd0dd5262390
Aabir Datta <dattaaabir13@gmail.com>
2022-04-30 12:15:23 +0530

    commit

---
+++ Easy/26.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int count=0;
+        for(int i=0; i<nums.length-1; i++) {
+            if(nums[i] == nums[i+1]) {
+                nums[i] = Integer.MAX_VALUE;
+                count += 1;
+            }
+        }
+        System.out.println(count);
+        Arrays.sort(nums);
+        return nums.length-count;
+    }
+}
